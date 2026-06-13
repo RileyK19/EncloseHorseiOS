1d168cbb13e0014b8db5f4be285590b99b705cb0
Riley Koo <rkoo@berkeley.edu>
2026-06-13 02:09:29 -0700

    Bump version for testflight

--- EncloseHorse/Constants.swift
+++ EncloseHorse/Constants.swift
@@ -5,10 +5,10 @@
 //  Created by Riley Koo on 5/1/26.
 //
 
-struct Constants {
-    static let baseURL = ""
-    // Left out url as I didn't get permission from the original owner
-
-     static let supabaseKey = ""
-    static let supabaseURL = ""
-}
+//struct Constants {
+//    static let baseURL = ""
+//    // Left out url as I didn't get permission from the original owner
+//
+//     static let supabaseKey = ""
+//    static let supabaseURL = ""
+//}
